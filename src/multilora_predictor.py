da6f5e4d9998ca7809087c1e6862c2e96cfe3011
hwelsters <hwelsters>
2024-07-20 16:13:39 -0700

    feat: thing.

--- src/multilora_predictor.py
+++ src/multilora_predictor.py
@@ -34,6 +34,7 @@ class MultiloraPredictor:
         matrixes = [matrix.astype(int) for matrix in matrixes] # Convert false to 0 and true to 1
         np_matrixes = np.array(matrixes)
         sum_matrix = np.sum(np_matrixes, axis=0) # element wise sum of matrixes
+        print(sum_matrix[0].shape)
         return sum_matrix[0]
 
     def calculate_average_uncertainty(matrixes: List[np.array]):
