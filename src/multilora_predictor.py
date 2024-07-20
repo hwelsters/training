0842f0af38e4d08d20e16cb8c63292d011714364
hwelsters <hwelsters>
2024-07-20 16:44:05 -0700

    feat: thing.

--- src/multilora_predictor.py
+++ src/multilora_predictor.py
@@ -7,18 +7,24 @@ from segment_anything_utils import save_black_and_white_image
 class MultiloraPredictor:
     def __init__(self, target_objects: List[str], model_checkpoint, weather, rank = 8):
         self.models = {}
+        self.model_checkpoint = model_checkpoint
+        self.weather = weather
+        self.rank = rank
+
         for target_object in target_objects:
             self.models[target_object] = LoraSamInference(model_checkpoint, f"finetuned_weights/{target_object}/{weather}/lora_rank{rank}.safetensors", rank)
 
     def predict(self, image_path, mask_path):
         outputs = {}
-        for target_object, model in self.models.items():
+        for target_object in self.models.keys():
+            model = LoraSamInference(self.model_checkpoint, f"finetuned_weights/{target_object}/{self.weather}/lora_rank{self.rank}.safetensors", self.rank)
             outputs[target_object] = model.predict(image_path, mask_path)
         return outputs
     
     def cached_predict(self, cache_path, image_path, mask_path):
         outputs = {}
-        for target_object, model in self.models.items():
+        for target_object in self.models.keys():
+            model = LoraSamInference(self.model_checkpoint, f"finetuned_weights/{target_object}/{self.weather}/lora_rank{self.rank}.safetensors", self.rank)
             outputs[target_object] = model.cached_predict(cache_path, image_path, mask_path)
         return outputs
     
@@ -34,7 +40,6 @@ class MultiloraPredictor:
         matrixes = [matrix.astype(int) for matrix in matrixes] # Convert false to 0 and true to 1
         np_matrixes = np.array(matrixes)
         sum_matrix = np.sum(np_matrixes, axis=0) # element wise sum of matrixes
-        print(sum_matrix[0].shape)
         return sum_matrix[0]
 
     def calculate_average_uncertainty(matrixes: List[np.array]):
